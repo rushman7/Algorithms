7005747c5c52956ed606053b939b801c4dc76a57
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-09-04 15:19:12 -0400

    stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,13 @@
 import argparse
 
 def find_max_profit(prices):
-  pass 
-
+  profit = prices[1] - prices[0] # price of day 2 - day 1 = profit
+  for index, i in enumerate(prices): # loop through all prices using enumerate for indexes
+    for p in prices[index + 1:]: # slicing the array
+      newprofit = p - i 
+      if newprofit > profit:
+        profit = newprofit
+  return profit
 
 if __name__ == '__main__':
   # You can test your implementation by running 
